61f14741b6e5721e082b26a42652c3cb02df39a8
Meghana-Kaligiri <133840578+Meghana-Kaligiri@users.noreply.github.com>
2023-06-19 14:48:10 +0530

    Add files via upload

---
+++ project4_rollingadie.py
@@ -0,0 +1,38 @@
+import random
+x="Y"
+while x=="y":
+    num = random.randint(1,6)
+if num==1:
+          print("[-------]")
+          print("[           ]")
+          print("[   0     ]")
+          print("[         ]")
+          print("[-------]")
+if num==2:
+         print("[--------]")
+         print("[    0     ]")
+         print("[            ]")
+         print("[      0    ]")
+         print("[---------]")
+if num==3:
+           print("[---------]")
+           print("[             ]")
+           print("[ 0  0  0 ]")
+           print("[             ]")
+           print("[----------]")
+if num==4:
+           print("[---------]")
+           print("[ 0      0]")
+           print("[            ]")
+           print("[             ]")
+           print("[  0      0 ]")
+           print("[----------]")
+if num==5:
+           print("[--------]")
+           print("[ 0 0 0 ]")
+           print("[          ]")
+           print("[ 0 0 0 ]")
+           print("[--------]")
+x=input("pressy to roll again and n to exit")
+print("\n")
+  
